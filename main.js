bfd49054676b338e3cf58cda48031f7600f027ee
Walker Milhoan <walker@milhoandesign.com>
2024-04-08 08:00:40 -0600

    Added error handling if a user does not enter a name

--- main.js
+++ main.js
@@ -3,8 +3,13 @@ const button = document.querySelector("button");
 
 function greet() {
     const name = prompt("What is your name?");
-    const greeting = document.querySelector("#greeting");
-    greeting.textContent = `Hello ${name}, nice to meet you!`;
+    
+    if (name) { // Check if the user entered a name
+        const greeting = document.querySelector("#greeting");
+        greeting.textContent = `Hello ${name}, nice to meet you!`;
+    } else {
+        alert("Please enter your name.");
+    }
 }
 
 button.addEventListener('click', greet); 
