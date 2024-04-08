6e072870526f2a0456386aa53739e7c0060811a8
Walker Milhoan <walker@milhoandesign.com>
2024-04-08 07:50:40 -0600

    Wrote a small greet function

---
+++ main.js
@@ -0,0 +1,10 @@
+
+const button = document.querySelector("button");
+
+function greet() {
+    const name = prompt("What is your name?");
+    const greeting = document.querySelector("#greeting");
+    greeting.textContent = `Hello ${name}, nice to meet you!`;
+}
+
+button.addEventListener('click', greet); 
